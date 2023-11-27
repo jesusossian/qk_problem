1d8df85a752d6b6d48dcbf6ff3c182f521e5fc6f
jesusossian <jesusossian@gmail.com>
2023-11-27 13:08:51 +0000

    updated

--- qkp_cpp_bc/adicionar_bc.cpp
+++ qkp_cpp_bc/adicionar_bc.cpp
@@ -257,307 +257,265 @@ int adicionar_bc_extender_quad(XPRSprob prob, Array<int> &mtype, Array<char> &qr
 
 }
 
-int adicionar_bc_lift(XPRSprob prob, Array<int> &mtype, Array<char> &qrtype, Array<double> &drhs, Array<int> &mstart, double objval_cover, Array<int> &ncols, Array<double> &dmatval, pxpress * probl)
-{
-  int i, card_cover, NC, t, q, ncut, card_lift, add_lift, nl, k;
-  double objval_lift;
-  Array<int> lmindex, lmstart(N), lncols(1);
-  Array<double> lobj(N), lrhs, ldmatval(N);
-  char var[64];
-  Array<char> cnames;
+int adicionar_bc_lift(XPRSprob prob, Array<int> &mtype, Array<char> &qrtype, Array<double> &drhs, Array<int> &mstart, double objval_cover, Array<int> &ncols, Array<double> &dmatval, pxpress * probl) {
+    int i, card_cover, NC, t, q, ncut, card_lift, add_lift, nl, k;
+    double objval_lift;
+    Array<int> lmindex, lmstart(N), lncols(1);
+    Array<double> lobj(N), lrhs, ldmatval(N);
+    char var[64];
+    Array<char> cnames;
   
-  Array<int> pd(N);
-  Array<int> wd(N);
-  Array<int> aux(N);
-  Array<int> wl(N);
-  Array<double> alpha;
+    Array<int> pd(N);
+    Array<int> wd(N);
+    Array<int> aux(N);
+    Array<int> wl(N);
+    Array<double> alpha;
   
-  int bd;
+    int bd;
   
-  card_cover = 0;
-  ncut = 0;
+    card_cover = 0;
+    ncut = 0;
   
-  if(XPRScreateprob(&probl->lift)) exit(9);
-  XPRSsetlogfile(probl->lift, "loglift.txt");  
-  modelo_lift(probl->lift);	  
+    if(XPRScreateprob(&probl->lift)) exit(9);
+    XPRSsetlogfile(probl->lift, "loglift.txt");  
+    modelo_lift(probl->lift);	  
   
-  for(i=0, k=0, NC=0; i<N; ++i) 
-    { 
-      if(z[i] <= 1-0.0001) 
-	{
-	  wl[NC] = W[i];
-	  aux[NC] = i;
-	  ++NC;
-	} 
-      else 
-	{
-	  pd[k] = 1;
-	  wd[k] = W[i];
-	  ++card_cover;
-	  ++k;
-	}
+    for(i=0, k=0, NC=0; i<N; ++i) { 
+        if(z[i] <= 1-0.0001) {
+	        wl[NC] = W[i];
+	        aux[NC] = i;
+	        ++NC;
+	    } 
+        else {
+	        pd[k] = 1;
+	        wd[k] = W[i];
+	        ++card_cover;
+	        ++k;
+	    }
     }
   
-  ordena_d(&wl, 0, NC, &aux);
+    ordena_d(&wl, 0, NC, &aux);
   
-  lrhs.aloca(1);
-  lmindex.aloca(1);
-  alpha.aloca(N);
+    lrhs.aloca(1);
+    lmindex.aloca(1);
+    alpha.aloca(N);
   
-  for(i=0; i<NC; ++i) 
-    {
-      alpha[aux[i]] = 0;
+    for(i=0; i<NC; ++i) {
+        alpha[aux[i]] = 0;
     }
   
-  for(t=0; t<NC; ++t) 
-    {
-      lrhs[0] = C-W[ aux[t] ];
-      lmindex[0] = 0;
-      if(XPRSchgrhs(probl->lift, 1, &lmindex, &lrhs)) exit(9);
+    for(t=0; t<NC; ++t) {
+        lrhs[0] = C-W[ aux[t] ];
+        lmindex[0] = 0;
+        if(XPRSchgrhs(probl->lift, 1, &lmindex, &lrhs)) exit(9);
+
+        if(pr_lp) XPRSwriteprob(probl->lift, "modelo_lift", "l");
+
+        XPRSmipoptimize(probl->lift, "");
+        XPRSgetmipsol(probl->lift, &xl, NULL);
+        XPRSgetdblattrib(probl->lift,XPRS_MIPOBJVAL, &objval_lift);
       
-      if(pr_lp)
-	XPRSwriteprob(probl->lift, "modelo_lift", "l");
+        lncols[0] = aux[t];
+        alpha[aux[t]] = card_cover-1-objval_lift;
       
-      XPRSmipoptimize(probl->lift, "");
-      XPRSgetmipsol(probl->lift, &xl, NULL);
-      XPRSgetdblattrib(probl->lift,XPRS_MIPOBJVAL, &objval_lift);
+        if(alpha[aux[t]] < -0.0001) {
+	        return 0;
+        }
+
+        if(alpha[aux[t]] < 0.0001) {
+            break;
+        }
       
-      lncols[0] = aux[t];
-      alpha[aux[t]] = card_cover-1-objval_lift;
+        if(XPRSchgobj(probl->lift, 1, &lncols, &alpha)) exit(9);
       
-      if(alpha[aux[t]] < -0.0001)
-	{
-	  return 0;
-	}
-      
-      if(alpha[aux[t]] < 0.0001) 
-	{
-	  break;
-	}
-      
-      if(XPRSchgobj(probl->lift, 1, &lncols, &alpha)) exit(9);
-      
-      if(XPRSchgcoef(probl->lift, 0, aux[t], W[ aux[t] ])) exit(9);
+        if(XPRSchgcoef(probl->lift, 0, aux[t], W[ aux[t] ])) exit(9);
     }
   
-  card_lift = 0;
-  for(i=0; i<N; ++i) 
-    {
-      if(z[i] > 1-0.0001) 
-	{
-	  dmatval[ card_lift ] = 1;
-	  ncols[ card_lift ] = i;
-	  ++card_lift;
-	} 
-      else if(alpha[i] > 0.0001)
-	{
-	  dmatval[ card_lift ] = alpha[i];
-	  ncols[ card_lift ] = i;
-	  ++card_lift;
-	}
+    card_lift = 0;
+    for(i=0; i<N; ++i) {
+        if(z[i] > 1-0.0001) {
+	        dmatval[ card_lift ] = 1;
+	        ncols[ card_lift ] = i;
+	        ++card_lift;
+	    } 
+        else if(alpha[i] > 0.0001) {
+            dmatval[ card_lift ] = alpha[i];
+            ncols[ card_lift ] = i;
+            ++card_lift;
+        }
     }
   
-  drhs[0] = card_cover-1;
-  mstart[1] = card_lift;
-  ncut++;
+    drhs[0] = card_cover-1;
+    mstart[1] = card_lift;
+    ncut++;
   
-  if(ncut > 0)
-    {
-      if(XPRSaddcuts(prob, ncut, &mtype, &qrtype, &drhs, &mstart, &ncols, &dmatval)) exit(9);
+    if(ncut > 0) {
+        if(XPRSaddcuts(prob, ncut, &mtype, &qrtype, &drhs, &mstart, &ncols, &dmatval)) exit(9);
     }
   
-  return ncut;
+    return ncut;
 }
 
-int adicionar_bc_lift_quad(XPRSprob prob, Array<int> &mtype, Array<char> &qrtype, Array<double> &drhs, Array<int> &mstart, double objval_cover, Array<int> &mcols, Array<double> &dmatval, pxpress * probl)
-{
-  int i, card_cover, NC, t, q, ncut, card_lift, add_lift, nl, k, l, j;
-  double objval_lift;
-  double left_lift;
-  Array<int> lmindex, lmstart(N), lncols(1);
-  Array<double> lobj(1), lrhs, ldmatval(N);
-  Array< Array<double> > y;
-  char var[64];
-  Array<char> cnames;
-  int w, p, nr, np, ad;
+int adicionar_bc_lift_quad(XPRSprob prob, Array<int> &mtype, Array<char> &qrtype, Array<double> &drhs, Array<int> &mstart, double objval_cover, Array<int> &mcols, Array<double> &dmatval, pxpress * probl) {
+    int i, card_cover, NC, t, q, ncut, card_lift, add_lift, nl, k, l, j;
+    double objval_lift;
+    double left_lift;
+    Array<int> lmindex, lmstart(N), lncols(1);
+    Array<double> lobj(1), lrhs, ldmatval(N);
+    Array< Array<double> > y;
+    char var[64];
+    Array<char> cnames;
+    int w, p, nr, np, ad;
 
-  Array<int> wd;
-  Array<int> pd;
-  int bd;
+    Array<int> wd;
+    Array<int> pd;
+    int bd;
 
-  y.aloca(N-1);
-  for(i=0; i<N-1; ++i)
-    y[i].redefine(N-i-1,0);
+    y.aloca(N-1);
+    for(i=0; i<N-1; ++i) y[i].redefine(N-i-1,0);
 
-  k=N;
-  for(i=0; i<N-1; ++i) 
-    {
-      for(j=0; j<N-i-1; ++j, ++k) 
-	{
-	  y[i][j] = x[k];
-	}
+    k=N;
+    for(i=0; i<N-1; ++i) {
+        for(j=0; j<N-i-1; ++j, ++k) {
+            y[i][j] = x[k];
+        }
     }
   
-  nr = N;
-  np = 2*N*(N-1)*(N-2);
-  mtype.redefine(nr, mtype[0]);
-  qrtype.redefine(nr,'L');
-  drhs.redefine(nr,0);
-  mstart.aloca(nr+1);
-  mcols.aloca(np);
-  dmatval.aloca(np);
+    nr = N;
+    np = 2*N*(N-1)*(N-2);
+    mtype.redefine(nr, mtype[0]);
+    qrtype.redefine(nr,'L');
+    drhs.redefine(nr,0);
+    mstart.aloca(nr+1);
+    mcols.aloca(np);
+    dmatval.aloca(np);
 
-  Array<int> aux(N);
-  Array<int> wl(N);
-  Array<double> alpha;
+    Array<int> aux(N);
+    Array<int> wl(N);
+    Array<double> alpha;
   
-  card_cover = 0;
-  ncut = 0;
+    card_cover = 0;
+    ncut = 0;
   
-  if(XPRScreateprob(&probl->lift)) exit(9);
+    if(XPRScreateprob(&probl->lift)) exit(9);
   
-  modelo_lift(probl->lift);	  
+    modelo_lift(probl->lift);	  
   
-  for(i=0, NC=0; i<N;++i) 
-    { 
-      if(z[i] < 1-0.0001) 
-	{
-	  wl[NC] = W[i];
-	  aux[NC] = i;
-	  ++NC;
-	} 
-      else 
-	{
-	  ++card_cover;
-	}
+    for(i=0, NC=0; i<N;++i) { 
+        if(z[i] < 1-0.0001) {
+            wl[NC] = W[i];
+            aux[NC] = i;
+            ++NC;
+        } 
+        else {
+            ++card_cover;
+        }
     }
 
-  ordena_d(&wl, 0, NC, &aux);
+    ordena_d(&wl, 0, NC, &aux);
 
-  lrhs.aloca(1);
-  lmindex.aloca(1);
-  alpha.aloca(N);
+    lrhs.aloca(1);
+    lmindex.aloca(1);
+    alpha.aloca(N);
   
-  for(i=0; i<NC; ++i) 
-    {
-      alpha[aux[i]] = 0;
+    for(i=0; i<NC; ++i) {
+        alpha[aux[i]] = 0;
     }
   
-  for(t=0; t<NC; ++t) 
-    {
-      lrhs[0] = C-W[ aux[t] ];
-      lmindex[0] = 0;
-      if(XPRSchgrhs(probl->lift, 1, &lmindex, &lrhs)) exit(9);
+    for(t=0; t<NC; ++t) {
+        lrhs[0] = C-W[ aux[t] ];
+        lmindex[0] = 0;
+        
+        if(XPRSchgrhs(probl->lift, 1, &lmindex, &lrhs)) exit(9);
       
-      if(pr_lp) XPRSwriteprob(probl->lift, "modelo_lift", "l");
+        if(pr_lp) XPRSwriteprob(probl->lift, "modelo_lift", "l");
       
-      XPRSmipoptimize(probl->lift, "");
-      XPRSgetmipsol(probl->lift, &xl, NULL);
-      XPRSgetdblattrib(probl->lift,XPRS_MIPOBJVAL, &objval_lift);
+        XPRSmipoptimize(probl->lift, "");
+        XPRSgetmipsol(probl->lift, &xl, NULL);
+        XPRSgetdblattrib(probl->lift,XPRS_MIPOBJVAL, &objval_lift);
       
-      lncols[0] = aux[t];
-      alpha[aux[t]] = lobj[0] = card_cover-1- objval_lift;
+        lncols[0] = aux[t];
+        alpha[aux[t]] = lobj[0] = card_cover-1- objval_lift;
       
-      if(alpha[aux[t]] < 0.0001) 
-	{
-	  break;
-	}
+        if(alpha[aux[t]] < 0.0001) {
+            break;
+        }
       
-      if(XPRSchgobj(probl->lift, 1, &lncols, &lobj)) exit(9);
+        if(XPRSchgobj(probl->lift, 1, &lncols, &lobj)) exit(9);
       
-      if(XPRSchgcoef(probl->lift, 0, aux[t], W[ aux[t] ])) exit(9);
+        if(XPRSchgcoef(probl->lift, 0, aux[t], W[ aux[t] ])) exit(9);
       
     }
   
-  mstart[0] = 0;
-  for(i=0, p=0, w=0; i<N; ++i)
-    {
-      left_lift = 0;
-      if(x[i] > 0.0001)
-	{
-	  for(j=0; j<N; ++j)
-	    {
-	      if(z[j]>1-0.0001) 
-		{
-		  if(i<j) left_lift += y[i][j-i-1];
-		  if(i>j) left_lift += y[j][i-j-1];
-		}
-	      else
-		{
-		  if(i<j) left_lift += alpha[j]*(y[i][j-i-1]);
-		  if(i>j) left_lift += alpha[j]*(y[j][i-j-1]);
-		}
-	    }
+    mstart[0] = 0;
+    for(i=0, p=0, w=0; i<N; ++i) {
+        left_lift = 0;
+        if(x[i] > 0.0001) {
+            for(j=0; j<N; ++j) {
+                if(z[j]>1-0.0001) {
+                    if(i<j) left_lift += y[i][j-i-1];
+	                if(i>j) left_lift += y[j][i-j-1];
+                }
+                else {
+	                if(i<j) left_lift += alpha[j]*(y[i][j-i-1]);
+	                if(i>j) left_lift += alpha[j]*(y[j][i-j-1]);
+	            }
+            }
 	  
-	  if(z[i]>1-0.0001) 
-	    {
-	      left_lift += (2-card_cover)*x[i];
-	    }
-	  else
-	    {
-	      left_lift += (alpha[i]+1-card_cover)*x[i];
-	    }
+            if(z[i]>1-0.0001) {
+                left_lift += (2-card_cover)*x[i];
+            }
+            else {
+                left_lift += (alpha[i]+1-card_cover)*x[i];
+            }
 	  
-	  if(left_lift > 0.0001) 
-	    {
-	      if(z[i]>1-0.0001) 
-		dmatval[w] = 2-card_cover; 
-	      else
-		dmatval[w] = alpha[i]+1-card_cover; 
+            if(left_lift > 0.0001) {
+                if(z[i]>1-0.0001) dmatval[w] = 2-card_cover; 
+                else dmatval[w] = alpha[i]+1-card_cover; 
 	      
-	      mcols[w] = i;
+                mcols[w] = i;
 	      
-	      for(j=0; j<N; ++j) 
-		{
-		  if(z[j]>1-0.0001)
-		    {
-		      if(i<j) 
-			{
-			  for(l=0, ad=N; l<i; ++l) ad += N-l-1;
-			  
-			  dmatval[++w] = 1; 
-			  mcols[w] = ad+j-i-1;
-			}
-		      if(i>j)
-			{
-			  for(l=0, ad=N; l<j; ++l) ad += N-l-1;
-			  
-			  dmatval[++w] = 1; 
-			  mcols[w] = ad+i-j-1; 
-			}
-		    }
-		  else 
-		    if(alpha[j] > 0.0001)
-		      {
-			if(i<j) 
-			  {
-			    for(l=0, ad=N; l<i; ++l) ad += N-l-1;
-			    
-			    dmatval[++w] = alpha[j]; 
-			    mcols[w] = ad+j-i-1;
-			  }
-			if(i>j) 
-			  {
-			    for(l=0, ad=N; l<j; ++l) ad += N-l-1;
-			    
-			    dmatval[++w] = alpha[j]; 
-			    mcols[w] = ad+i-j-1;
-			  }
-		      }
-		}
-	      
-	      mstart[++p] = ++w;
-	      ncut++;
-	    }//end if(left_lift > 0.0001)
-	}//end if(x[i] > 0.0001)
+                for(j=0; j<N; ++j) {
+                    if(z[j]>1-0.0001) {
+                        if(i<j) {
+                            for(l=0, ad=N; l<i; ++l) ad += N-l-1;
+
+                            dmatval[++w] = 1; 
+                            mcols[w] = ad+j-i-1;
+                        }
+    	                if(i>j) {
+                            for(l=0, ad=N; l<j; ++l) ad += N-l-1;
+
+                            dmatval[++w] = 1; 
+                            mcols[w] = ad+i-j-1; 
+                        }
+                    }
+                    else if(alpha[j] > 0.0001) {
+	                    if(i<j) {
+                            for(l=0, ad=N; l<i; ++l) ad += N-l-1;
+    
+                            dmatval[++w] = alpha[j]; 
+                            mcols[w] = ad+j-i-1;
+	                    }
+                        if(i>j) {
+                            for(l=0, ad=N; l<j; ++l) ad += N-l-1;
+                            dmatval[++w] = alpha[j]; 
+                            mcols[w] = ad+i-j-1;
+                        }
+                    }
+                }
+
+                mstart[++p] = ++w;
+                ncut++;
+	        } //end if(left_lift > 0.0001)
+	    } //end if(x[i] > 0.0001)
     }//end for(i = ..)
   
-  if(ncut>0)
-    {
-      if(XPRSaddcuts(prob, ncut, &mtype, &qrtype, &drhs, &mstart, &mcols, &dmatval)) exit(9);
+    if(ncut>0) {
+        if(XPRSaddcuts(prob, ncut, &mtype, &qrtype, &drhs, &mstart, &mcols, &dmatval)) exit(9);
     }
   
-  return ncut;
+    return ncut;
 }
 
 
